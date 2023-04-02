3d9323de0e37cb1b2bf238abdd9669e18feb5a91
Mostafijur-Rahman <mosta5617@gmail.com>
2023-04-02 23:28:24 +0600

    Pending orders shows

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function index(){
-        return view('admin.pendingorders');
+        $status='Pending';
+        $products=Order::where('status', $status)->get();
+        return view('admin.pendingorders', compact('products')); 
     }
 
 }
--- resources/views/admin/pendingorders.blade.php
+++ resources/views/admin/pendingorders.blade.php
@@ -8,6 +8,49 @@
 
 @section('content')
     
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempora nostrum dolore quasi, dolorem minus odio quisquam dolor veritatis, quis, facilis accusamus. Non rem, repellendus accusamus at consequuntur autem quam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est facere quidem blanditiis distinctio quae tenetur sapiente magnam quasi rem porro? Necessitatibus blanditiis beatae, soluta veritatis dolorum sed praesentium illum autem? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi explicabo vel debitis sunt, ut fugiat animi excepturi, assumenda delectus alias eligendi reiciendis nostrum quasi tempora nihil veniam exercitationem perspiciatis cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempora nostrum dolore quasi, dolorem minus odio quisquam dolor veritatis, quis, facilis accusamus. Non rem, repellendus accusamus at consequuntur autem quam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est facere quidem blanditiis distinctio quae tenetur sapiente magnam quasi rem porro? Necessitatibus blanditiis beatae, soluta veritatis dolorum sed praesentium illum autem? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi explicabo vel debitis sunt, ut fugiat animi excepturi, assumenda delectus alias eligendi reiciendis nostrum quasi tempora nihil veniam exercitationem perspiciatis cupiditate.Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempora nostrum dolore quasi, dolorem minus odio quisquam dolor veritatis, quis, facilis accusamus. Non rem, repellendus accusamus at consequuntur autem quam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est facere quidem blanditiis distinctio quae tenetur sapiente magnam quasi rem porro? Necessitatibus blanditiis beatae, soluta veritatis dolorum sed praesentium illum autem? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi explicabo vel debitis sunt, ut fugiat animi excepturi, assumenda delectus alias eligendi reiciendis nostrum quasi tempora nihil veniam exercitationem perspiciatis cupiditate.
+<div class="container">
+    <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">SL#</th>
+            <th scope="col">Product Name</th>
+            <th scope="col">Address</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Price</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            @php
+                $i=0;
+            @endphp
+          @foreach ($products as $product )
+          <tr>
+            @php
+                 $product_name=App\Models\Product::where('id', $product->product_id )->value('product_name');
+                 $i++;
+             @endphp
+            <th scope="row"> {{ $i }} </th>
+            <td>{{ $product_name }}</td> 
+            <td>
+                {{ $product->address }}
+                {{ $product->postalcode }}
+            </td> 
+            <td>{{ $product->phonenumber }}</td> 
+            <td>{{ $product->total_price }}</td> 
+            <td><span class="badge badge-pill badge-primary">{{  $product->product_quantity }}</span></td>
+
+            <td>
+                <a href="{{ route('editproduct', $product->id ) }}" class="btn btn-primary btn-sm">Approve</a>
+                
+                <a href="{{ route('deleteproduct', $product->id ) }}" class="btn btn-danger btn-sm">Cancle</a>
+            </td>
+          </tr>
+                                
+          @endforeach
+        </tbody>
+      </table>
+</div>
 
 @endsection

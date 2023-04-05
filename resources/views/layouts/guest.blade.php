63bc0932193ff4a8850d8ec0da74850c3430c7c3
Mostafijur-Rahman <mosta5617@gmail.com>
2023-04-06 00:03:43 +0600

    user login page updated

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,6 +16,7 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+           <h1> User Login </h1>
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
